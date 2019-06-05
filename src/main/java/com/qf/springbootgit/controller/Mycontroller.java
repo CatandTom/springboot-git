1b56100d4965454aa6cd06a9e5839566bdda242f
gbg <1139248418@qq.com>
2019-06-05 17:42:40 +0800

    Initial commit

---
+++ src/main/java/com/qf/springbootgit/controller/Mycontroller.java
@@ -0,0 +1,14 @@
+package com.qf.springbootgit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Gbg
+ * @Date 2019/6/5
+ */
+@Controller
+@RequestMapping("my")
+public class Mycontroller {
+
+}
